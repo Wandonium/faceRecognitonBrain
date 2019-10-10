8cb75da7bd9bb99833014858693941a83d3b0db5
Wandonium <hillarywando@gmail.com>
2019-10-10 13:10:16 +0300

    Updates

---
+++ precache-manifest.89122da72c1675025cf2a68558fa7387.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ffa2e4fa5f6b303cfec4",
+    "url": "/faceRecognitonBrain/static/js/runtime~main.ffa2e4fa.js"
+  },
+  {
+    "revision": "c4f4f8216ea7cb1d167a",
+    "url": "/faceRecognitonBrain/static/js/main.c4f4f821.chunk.js"
+  },
+  {
+    "revision": "ba1ba957a59664094964",
+    "url": "/faceRecognitonBrain/static/js/2.ba1ba957.chunk.js"
+  },
+  {
+    "revision": "c4f4f8216ea7cb1d167a",
+    "url": "/faceRecognitonBrain/static/css/main.bf57fb17.chunk.css"
+  },
+  {
+    "revision": "92eaa628d49f3d9072c9d6346a04c646",
+    "url": "/faceRecognitonBrain/index.html"
+  }
+];
